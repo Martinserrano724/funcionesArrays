bd7a94036912b3f933f30c7c1ad92b4e1255c1aa
Martinserrano724 <martinserrano724@gmail.com>
2023-03-13 01:05:32 -0300

    Desarrollo del ejercicio funciones 2

--- js/ejerFunciones2.js
+++ js/ejerFunciones2.js
@@ -2,3 +2,38 @@
 que se le pasa como argumento. A partir de la cadena que se le pasa, 
 la función determina si esa cadena está formada sólo 
 por mayúsculas, sólo por minúsculas o por una mezcla de ambas.*/
+
+let contadorMayuscula=0;
+let contadorMinuscula=0;
+
+const mayusculaMinuscula =function (letra)
+{
+     if(letra === letra.toUpperCase()){
+        contadorMayuscula++;
+     }
+     if( letra === letra.toLowerCase()){
+        contadorMinuscula++;
+     }
+     
+}
+const controlar=function(frase){
+    if (contadorMayuscula == 0 && contadorMinuscula > 0 ) {
+        document.write( `La cadena: <b>${frase}</b> es todo minuscula`);
+    }
+    else if (contadorMayuscula > 0 && contadorMinuscula == 0) {
+        document.write( `La cadena: <b>${frase}</b> es todo Mayuscula`) ;
+    }
+    else  {
+        document.write( `La cadena: <b>${frase}</b> es Mescla`) ;
+    }
+}
+  
+
+
+let frase = prompt('ingresa una frase ');
+for(let index = 0; index < frase.length; index++)
+{
+    mayusculaMinuscula(frase[index]);     
+}
+controlar(frase);
+
