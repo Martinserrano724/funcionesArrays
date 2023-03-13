a3124567e8326e1a1a719e34420bc82800697561
Martinserrano724 <martinserrano724@gmail.com>
2023-03-13 00:47:21 -0300

    Desarrollo del ejercicio funciones 1

--- js/ejerFunciones1.js
+++ js/ejerFunciones1.js
@@ -3,3 +3,25 @@
   de texto que indica si el número es par o impar. Mostrar por pantalla
    el resultado devuelto por la función.
 */
+
+const parImpar = function(num){
+    if((num %2)==0){
+        document.write(`El numero: ${num} es par `);
+    }
+    else{
+        document.write(`El numero: ${num} es impar `);
+    }
+};
+let bandera = 0;
+let numero1 ;
+while (bandera == 0){
+     numero1=parseInt(prompt('Ingrese un numero'));
+    if( isNaN(numero1)) {
+       alert(`ERROR... Ingresar un numero  `);
+    }
+    else{
+        bandera ++;
+    }
+
+}
+parImpar(numero1);
