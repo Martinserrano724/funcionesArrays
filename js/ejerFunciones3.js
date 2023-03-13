380399a83c006f79316b18bbc8af6a9f44d70a8d
Martinserrano724 <martinserrano724@gmail.com>
2023-03-13 09:49:27 -0300

    Desarrollo de la funciones del ejercicio 3 de funciones

--- js/ejerFunciones3.js
+++ js/ejerFunciones3.js
@@ -3,3 +3,28 @@ luego crear una función para calcular su perímetro y mostrarlo por pantalla.
 
 La fórmula del perímetro  es p = 2*(a +b)*/
 
+let ladoA;
+let ladoB;
+let bandera = 0;
+
+const validar = function(){
+    while (bandera == 0){
+        ladoA= parseInt(prompt(`Ingrese el lado 1 del rectangulo `));
+        ladoB= parseInt(prompt(`Ingrese el lado 2 del rectangulo `));
+        if( isNaN(ladoA) && isNaN(ladoB) || isNaN(ladoA) && !isNaN(ladoB) || !isNaN(ladoA) && isNaN(ladoB)) {
+           alert(`ERROR... Ingresar un numero  `);
+        }
+        else{
+            bandera ++;
+        }
+    
+    }
+}
+ function perimetro(a , b){
+    let p ;
+    p = 2*(a +b)
+    return p;
+}
+validar();
+const resultado=perimetro(ladoA , ladoB);
+document.write(`El resultado es: ${resultado}`);
