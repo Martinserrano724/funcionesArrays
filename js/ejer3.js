a6195af0b5eea16572964e4f2566ff7a589120d5
Martinserrano724 <martinserrano724@gmail.com>
2023-03-13 09:33:59 -0300

    Correccion del ejercicio 3 de arrays agregando el mostrar repeticiones de cada suma

--- js/ejer3.js
+++ js/ejer3.js
@@ -15,6 +15,93 @@ for (let index = 0; index < 50; index++) {
     jugadas[index]=(numero1+numero2);
 }
 document.write(`Las jugadas son: <br>`);
+let contador2 =0;
+let contador3 =0;
+let contador4 =0;
+let contador5 =0;
+let contador6 =0;
+let contador7 =0;
+let contador8 =0;
+let contador9 =0;
+let contador10 =0;
+let contador11=0;
+let contador12=0;
 for (let index = 0; index < jugadas.length; index++) {
     document.write(`${jugadas[index]} -`);
+    
 }
+for (let index = 0; index < jugadas.length; index++) {
+    if (jugadas[index] == 2){
+        contador2++;
+    }
+    else if(jugadas[index]==3){
+        contador3++;
+    }
+    else if(jugadas[index]==4){
+        contador4++;
+    }
+    else if(jugadas[index]==5){
+        contador5++;
+    }
+    else if(jugadas[index]==6){
+        contador6++;
+    }
+    else if(jugadas[index]==7){
+        contador7++;
+    }
+    else if(jugadas[index]==8){
+        contador8++;
+    }
+    else if(jugadas[index]==9){
+        contador9++;
+    }
+    else if(jugadas[index]==10){
+        contador10++;
+    }
+    else if(jugadas[index]==11){
+        contador11++;
+    }
+    else if(jugadas[index]==12){
+        contador12++;
+    }
+    document.write(`${jugadas[index]} -`);
+    
+    
+}
+
+document.write(`<br>la cantidad de veces que se repitieron los tiros
+<ul class = "lista">
+    <li>
+        2=${contador2} 
+    </li>
+    <li>
+        3=${contador3} 
+    </li>
+    <li>
+        4=${contador4} 
+    </li>
+    <li>
+        5=${contador5} 
+    </li>
+    <li>
+        6=${contador6} 
+    </li>
+    <li>
+        7=${contador7} 
+    </li>
+    <li>
+        8=${contador8}
+    </li>
+    <li>
+        9=${contador9} 
+    </li>
+    <li>
+        10=${contador10} 
+    </li>
+    <li>
+        11=${contador11} 
+    </li>
+    <li>
+        12=${contador12} 
+    </li>
+</ul>`);
